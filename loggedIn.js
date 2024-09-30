3ebbe3dbf144566585f34184d5d2b2ad4de43513
Karl Vincent Paclar <paclarvincentkarl@gmail.com>
2024-10-01 02:48:25 +0800

    Delete

--- loggedIn.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const userEmail = localStorage.getItem('userEmail');
-
-    if (userEmail) {
-        document.getElementById('userEmailPlaceholder').textContent = userEmail;
-    } else {
-        document.getElementById('userEmailPlaceholder').textContent = 'No email found';
-    }
-});
